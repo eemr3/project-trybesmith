d7f05ca441ef9a63d49a6bd27008c8c3a0427975
Emerson <eemr3@yahoo.com.br>
2022-05-13 18:31:18 -0300

    feat: adiciona a rota de product o metodo post

--- src/routes/Products.routes.ts
+++ src/routes/Products.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import ProductController from '../controllers/Product.controller';
+import ProductMiddleware from '../middlewares/Products.middleware';
 
 const routes = Router();
 const productController = new ProductController();
+const productMiddleware = new ProductMiddleware();
 
 routes.get('/products', productController.getAll);
+routes.post('/products', productMiddleware.validationProduct, productController.create);
 
 export default routes;
