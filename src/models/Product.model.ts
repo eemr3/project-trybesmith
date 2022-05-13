99c8e64c943bdf39276b68c35ef613442ab59b13
Emerson <eemr3@yahoo.com.br>
2022-05-13 18:29:03 -0300

    feat: adiciona ao modelo de product o metodo create

--- src/models/Product.model.ts
+++ src/models/Product.model.ts
@@ -1,3 +1,4 @@
+import { ResultSetHeader } from 'mysql2/promise';
 import connection from './connection';
 import IProduct from '../../interfaces/Product.interface';
 
@@ -6,5 +7,18 @@ export default class ProductModel {
     const [result] = await connection.execute('SELECT * FROM Trybesmith.Products');
 
     return result as IProduct[];
+  };
+
+  public create = async (name: string, amount: string): Promise<IProduct> => {
+    const [product] = await connection.execute<ResultSetHeader>(
+      'INSERT INTO Trybesmith.Products (name, amount) VALUES(?,?)',
+      [name, amount],
+    );
+
+    return {
+      id: product.insertId,
+      name,
+      amount,
+    };
   };
 }
