53195a2a016d51855bb584c2949fd7d33bb0074a
Emerson <eemr3@yahoo.com.br>
2022-05-13 21:14:02 -0300

    feat: adiciona rota de user ano app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import ProductRoute from './routes/Products.routes';
+import { ProductRoutes, UserRoutes } from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.use(ProductRoute);
+app.use(ProductRoutes);
+app.use(UserRoutes);
 
 export default app;
