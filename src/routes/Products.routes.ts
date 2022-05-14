9eafd3846ac439ab75fb238308fd34f5bc832a79
Emerson <eemr3@yahoo.com.br>
2022-05-13 21:14:21 -0300

    autera arquivos

--- src/routes/Products.routes.ts
+++ src/routes/Products.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import ProductController from '../controllers/Product.controller';
-import ProductMiddleware from '../middlewares/Products.middleware';
+import ValidationMiddleware from '../middlewares/Validation.middleware';
 
 const routes = Router();
 const productController = new ProductController();
-const productMiddleware = new ProductMiddleware();
+const productMiddleware = new ValidationMiddleware();
 
 routes.get('/products', productController.getAll);
 routes.post('/products', productMiddleware.validationProduct, productController.create);
