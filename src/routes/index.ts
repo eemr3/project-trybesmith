36d1881a4be06ce4e61de84ac6c0aaa9fb38199c
Emerson <eemr3@yahoo.com.br>
2022-05-14 22:12:06 -0300

    feat: adicona rota OrderRoutes e exporta a mesma

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,5 @@
 import ProductRoutes from './Products.routes';
 import UserRoutes from './User.routes';
+import OrderRoutes from './Orders.routes';
 
-export { ProductRoutes, UserRoutes };
+export { ProductRoutes, UserRoutes, OrderRoutes };
