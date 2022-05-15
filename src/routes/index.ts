93b28f0eb448749b834a80528b95ca6093fbf310
Emerson <eemr3@yahoo.com.br>
2022-05-14 23:20:08 -0300

    feat: adiciona rota de login LoginRoutes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import ProductRoutes from './Products.routes';
 import UserRoutes from './User.routes';
 import OrderRoutes from './Orders.routes';
+import LoginRoutes from './Login.routes';
 
-export { ProductRoutes, UserRoutes, OrderRoutes };
+export { ProductRoutes, UserRoutes, OrderRoutes, LoginRoutes };
