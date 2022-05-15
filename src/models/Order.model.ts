9b8d8fd2da508a3a84db62339b98ba6d6869cd5c
Emerson <eemr3@yahoo.com.br>
2022-05-14 22:08:31 -0300

    feat: cria model Order.model.ts com medtodo getAll

---
+++ src/models/Order.model.ts
@@ -0,0 +1,22 @@
+import connection from './connection';
+import { IOrder, IProdOrder } from '../../interfaces/Order.interface';
+
+export default class OrderModel {
+  public getAll = async (): Promise<IOrder[]> => {
+    const [orders] = await connection.execute(`SELECT od.id as id, us.id as usId
+    FROM Trybesmith.Orders as od
+        INNER JOIN Trybesmith.Users as us
+        ON od.userId  = us.id`);
+
+    return orders as IOrder[];
+  };
+
+  public getAllProductsIds = async (): Promise<IProdOrder[]> => {
+    const [result] = await connection.execute(`SELECT pd.id as prdId, o.id as ordId
+    FROM Trybesmith.Products as pd
+    INNER JOIN Trybesmith.Orders as o
+    ON o.id = pd.orderId`);
+
+    return result as IProdOrder[];
+  };
+}
