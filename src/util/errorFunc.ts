bac26ca66db84115d00173e4da95b414eef2fbd8
Emerson <eemr3@yahoo.com.br>
2022-05-14 23:16:15 -0300

    feat: criar class CustonError com metodo errro

---
+++ src/util/errorFunc.ts
@@ -0,0 +1,16 @@
+export default class CustonError extends Error {
+  status: number;
+
+  message: string;
+  
+  constructor(status: number, message: string) {
+    super();
+    this.status = status;
+    this.message = message;
+  }
+
+  public error = () => ({
+    status: this.status,
+    message: this.message,
+  });
+}
